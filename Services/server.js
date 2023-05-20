c54be76e31a57583d6d9cd7d51b69c7f4bc483c1
aych <aych@aychs-MacBook-Pro.local>
2023-05-20 13:13:40 -0400

    backend server added

---
+++ Services/server.js
@@ -0,0 +1,32 @@
+const PORT = process.env.PORT || 8000;
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.listen(PORT, () => console.log('Server is running'));
+
+app.get('/games', async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.rawg.io/api/games?key=${process.env.API_KEY}`
+    );
+    res.json(data);
+  } catch (err) {
+    console.error('Error in games');
+  }
+});
+
+app.get('/genres', async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.rawg.io/api/genres?key=${process.env.API_KEY}`
+    );
+    res.json(data);
+  } catch (err) {
+    console.error('Error in genres');
+  }
+});
